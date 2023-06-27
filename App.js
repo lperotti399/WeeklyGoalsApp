2da82c200dc198b49bdfcc161285efc359d38238
Luch <lperotti39@gmail.com>
2023-06-27 09:58:36 -0400

    Created new react-native template

---
+++ App.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View>
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
